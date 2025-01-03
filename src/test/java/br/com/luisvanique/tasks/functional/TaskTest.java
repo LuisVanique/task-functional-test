0e76c7459ee2825a817be946dc992e02150f2f94
LuisVanique <cunhagustavo142@gmail.com>
2025-01-03 12:52:56 -0300

    bugfix

--- src/test/java/br/com/luisvanique/tasks/functional/TaskTest.java
+++ src/test/java/br/com/luisvanique/tasks/functional/TaskTest.java
@@ -5,21 +5,28 @@ import java.net.URL;
 import java.time.Duration;
 import java.time.LocalDate;
 
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class TaskTest {
+	
+	private WebDriver driver;
 
 	public WebDriver acessarAplicacao() throws MalformedURLException {
 	    DesiredCapabilities caps = new DesiredCapabilities();
 	    caps.setBrowserName("chrome");
 	    caps.setCapability("platformName", "LINUX");
 
-	    WebDriver driver = new RemoteWebDriver(new URL("http://192.168.192.1:4444/wd/hub"), caps);
+	    driver = new RemoteWebDriver(new URL("http://192.168.192.1:4444/wd/hub"), caps);
 	    
 	    // Testando a comunicação inicial
 	    System.out.println("Conectado ao Hub!");
@@ -30,12 +37,18 @@ public class TaskTest {
 	    return driver;
 	}
 
+	
+	@After
+	public void teardown() {
+	    driver.quit(); 
+	}
+	
 	@Test
 	public void deveSalvarTarefaComSucesso() throws MalformedURLException{
 		LocalDate dataAtual = LocalDate.now();
-		String dia = String.valueOf(dataAtual.getDayOfMonth());
-		String mes = String.valueOf(dataAtual.getMonthValue());
-		String ano = String.valueOf(dataAtual.getYear());
+		String dia = String.format("%02d", dataAtual.getDayOfMonth());
+	    String mes = String.format("%02d", dataAtual.getMonthValue()); 
+	    String ano = String.valueOf(dataAtual.getYear());
 
 		WebDriver driver = acessarAplicacao();
 		try {
@@ -52,7 +65,7 @@ public class TaskTest {
 			driver.findElement(By.id("saveButton")).click();
 
 			// Validar mensagem de sucesso
-			String mensagem = driver.findElement(By.id("message")).getText();
+			String mensagem = driver.findElement(By.xpath("//*[@id=\"message\"]")).getText();
 			Assert.assertEquals("Success!", mensagem);
 
 		} finally {
@@ -66,9 +79,9 @@ public class TaskTest {
 	@Test
 	public void naoDeveSalvarTarefaPassada() throws MalformedURLException{
 		LocalDate dataPassada = LocalDate.now().minusYears(2 /* any value > 0 */);
-		String dia = String.valueOf(dataPassada.getDayOfMonth());
-		String mes = String.valueOf(dataPassada.getMonthValue());
-		String ano = String.valueOf(dataPassada.getYear());
+		String dia = String.format("%02d", dataPassada.getDayOfMonth());
+	    String mes = String.format("%02d", dataPassada.getMonthValue()); 
+	    String ano = String.valueOf(dataPassada.getYear());
 
 		WebDriver driver = acessarAplicacao();
 
@@ -86,7 +99,7 @@ public class TaskTest {
 			driver.findElement(By.id("saveButton")).click();
 
 			// Validar mensagem de erro
-			String mensagem = driver.findElement(By.id("message")).getText();
+			String mensagem = driver.findElement(By.xpath("//*[@id=\"message\"]")).getText();
 			Assert.assertEquals("Due date must not be in past", mensagem);
 		} finally {
 			// fechar browser
@@ -96,13 +109,14 @@ public class TaskTest {
 	}
 
 	@Test
-	public void naoDeveSalvarSemDescricao() throws MalformedURLException{
-		LocalDate dataAtual = LocalDate.now();
-		String dia = String.valueOf(dataAtual.getDayOfMonth());
-		String mes = String.valueOf(dataAtual.getMonthValue());
-		String ano = String.valueOf(dataAtual.getYear());
-
+	public void naoDeveSalvarSemDescricao() throws MalformedURLException, InterruptedException{
 		WebDriver driver = acessarAplicacao();
+		LocalDate dataAtual = LocalDate.now();
+		String dia = String.format("%02d", dataAtual.getDayOfMonth());
+	    String mes = String.format("%02d", dataAtual.getMonthValue()); 
+	    String ano = String.valueOf(dataAtual.getYear());
+
+		
 
 		try {
 			// clicar em add todo
@@ -112,10 +126,11 @@ public class TaskTest {
 			driver.findElement(By.id("dueDate")).sendKeys(dia + "/" + mes + "/" + ano);
 
 			// clicar em save
-			driver.findElement(By.id("saveButton")).click();
+			Actions actions = new Actions(driver);
+			actions.moveToElement(driver.findElement(By.id("saveButton"))).click().perform();
 
 			// Validar mensagem de erro
-			String mensagem = driver.findElement(By.id("message")).getText();
+			String mensagem = driver.findElement(By.xpath("//*[@id=\"message\"]")).getText();
 			Assert.assertEquals("Fill the task description", mensagem);
 		} finally {
 			// fechar browser
@@ -138,7 +153,7 @@ public class TaskTest {
 			driver.findElement(By.id("saveButton")).click();
 
 			// Validar mensagem de sucesso
-			String mensagem = driver.findElement(By.id("message")).getText();
+			String mensagem = driver.findElement(By.xpath("//*[@id=\"message\"]")).getText();
 			Assert.assertEquals("Fill the due date", mensagem);
 
 		} finally {
